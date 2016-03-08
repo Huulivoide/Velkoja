3c73b503652584034ea2a3af9405d6954152a893
Jesse Jaara <jesse.jaara@gmail.com>
2016-03-08 16:01:28 +0200

    Add debt class for representing debts table rows.

---
+++ velkoja/src/main/java/fi/huulivoide/velkoja/Debt.java
@@ -0,0 +1,23 @@
+package fi.huulivoide.velkoja;
+
+import android.support.annotation.NonNull;
+
+import java.util.Calendar;
+
+public class Debt {
+    public final long id;
+    public final String description;
+    public final double sum;
+    public final Calendar due;
+    public final Calendar paid;
+    public final long person;
+
+    public Debt(long id, @NonNull String description, double sum, @NonNull Calendar due, Calendar paid, long person) {
+        this.id = id;
+        this.description = description;
+        this.sum = sum;
+        this.due = due;
+        this.paid = paid;
+        this.person = person;
+    }
+}
