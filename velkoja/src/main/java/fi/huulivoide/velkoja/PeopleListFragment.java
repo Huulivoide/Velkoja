ed1aa64deade4289ec7b88d949375700e536a9c3
Jesse Jaara <jesse.jaara@gmail.com>
2016-03-08 17:17:06 +0200

    Add support for opening PersonFragment and deleting people from PeopleListfragment.

--- velkoja/src/main/java/fi/huulivoide/velkoja/PeopleListFragment.java
+++ velkoja/src/main/java/fi/huulivoide/velkoja/PeopleListFragment.java
@@ -3,6 +3,7 @@ package fi.huulivoide.velkoja;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -87,11 +88,38 @@ public class PeopleListFragment extends BackHandledFragment
                 .commit();
     }
 
+    private void deletePerson(long id) {
+        Person deleted = mPeople.delete(id);
+        Snackbar undo = Snackbar.make(getView(), R.string.person_deleted, Snackbar.LENGTH_INDEFINITE);
+        undo.setAction(R.string.undo, (v) -> {
+            mPeople.insert(deleted);
+            searchQueryListener.onQueryTextChange("");
+        });
+        searchQueryListener.onQueryTextChange("");
+        undo.show();
+    }
+
+    private void showPerson(View view) {
+        PersonFragment frag = new PersonFragment_();
+        Bundle args = new Bundle();
+        args.putLong("id", ((PersonItemView) view).getPersonId());
+        frag.setArguments(args);
+
+        getFragmentManager()
+            .beginTransaction()
+            .setCustomAnimations(R.animator.enter_from_left, R.animator.exit_to_right,
+                    R.animator.enter_from_right, R.animator.exit_to_left)
+            .replace(R.id.content_frame, frag)
+            .addToBackStack(null)
+            .commit();
+    }
+
     /**
      * Initialize and decorate the actual list widget.
      */
     private void setupList() {
         mAdapter = mPeople.getAdapter();
+        mAdapter.setOnClickListener(this::showPerson);
 
         mList.setHasFixedSize(true);
         mList.addItemDecoration(new DividerItemDecoration(mContext, null));
@@ -141,6 +169,18 @@ public class PeopleListFragment extends BackHandledFragment
         setupToolbar();
 
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        VelkojaActivity activity = (VelkojaActivity) getActivity();
+        Long person = activity.personQueuedForDeletion();
+        if (person != null) {
+            deletePerson(person);
+            activity.clearDeletionQueue();
+        }
     }
 
     @Override
