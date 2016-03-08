2abe3cb34af574ff0d7bbb083d46b6eddb6a06c2
Jesse Jaara <jesse.jaara@gmail.com>
2016-03-08 15:58:29 +0200

    Add support clickactions in people list.

--- velkoja/src/main/java/fi/huulivoide/velkoja/PeopleAdapter.java
+++ velkoja/src/main/java/fi/huulivoide/velkoja/PeopleAdapter.java
@@ -3,15 +3,20 @@ package fi.huulivoide.velkoja;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
+import android.view.View;
 import android.view.ViewGroup;
 
 public class PeopleAdapter extends RecyclerViewCursorAdapterBase<PersonItemView>
 {
     Context mContext;
+    View.OnClickListener mClickListener;
 
     @Override
     protected PersonItemView onCreateItemView(ViewGroup parent, int viewType) {
-        return PersonItemView_.build(mContext);
+        PersonItemView v = PersonItemView_.build(mContext);
+        v.setOnClickListener(mClickListener);
+
+        return v;
     }
 
     public PeopleAdapter(@NonNull Context context, @NonNull Cursor cursor) {
@@ -24,6 +29,16 @@ public class PeopleAdapter extends RecyclerViewCursorAdapterBase<PersonItemView>
         PersonItemView view = viewHolder.getView();
         mCursor.moveToPosition(position);
 
-        view.bind(mCursor.getString(1), mCursor.getString(2), mCursor.getString(3));
+        view.bind(mCursor.getLong(0), mCursor.getString(1), mCursor.getString(2), mCursor.getString(3));
+    }
+
+    /**
+     * Associate the given click listener with all views contained in the list.
+     * Must be set before the adapter is handed over to the RecyclerView
+     *
+     * @param listener
+     */
+    public void setOnClickListener(View.OnClickListener listener) {
+        mClickListener = listener;
     }
 }
--- velkoja/src/main/java/fi/huulivoide/velkoja/PersonItemView.java
+++ velkoja/src/main/java/fi/huulivoide/velkoja/PersonItemView.java
@@ -2,6 +2,7 @@ package fi.huulivoide.velkoja;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.MotionEvent;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,6 +12,8 @@ import org.androidannotations.annotations.ViewById;
 @EViewGroup(R.layout.person_item)
 public class PersonItemView extends LinearLayout
 {
+    private long mId;
+
     @ViewById(R.id.name_text)
     protected TextView mNameText;
 
@@ -20,13 +23,24 @@ public class PersonItemView extends LinearLayout
     @ViewById(R.id.bic_text)
     protected TextView mBicText;
 
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return true;
+    }
+
     public PersonItemView(Context context) {
         super(context);
     }
 
-    public void bind(@NonNull String name, @NonNull String iban, @NonNull String bic) {
+    public void bind(long id, @NonNull String name, @NonNull String iban, @NonNull String bic) {
+        mId = id;
+
         mNameText.setText(name);
         mIbanText.setText(iban);
         mBicText.setText(bic);
+    }
+
+    public long getPersonId() {
+        return mId;
     }
 }
