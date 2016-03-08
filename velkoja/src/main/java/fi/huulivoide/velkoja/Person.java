d5c36d20e72985f190897f049bca0fa771cb0e49
Jesse Jaara <jesse.jaara@gmail.com>
2016-03-08 17:01:16 +0200

    Update Person class to include debts too.

--- velkoja/src/main/java/fi/huulivoide/velkoja/Person.java
+++ velkoja/src/main/java/fi/huulivoide/velkoja/Person.java
@@ -2,18 +2,24 @@ package fi.huulivoide.velkoja;
 
 import android.support.annotation.NonNull;
 
+import java.util.List;
+
 public class Person
 {
     public final long id;
     public final String name;
     public final String iban;
     public final String bic;
+    public final List<Debt> unpaid;
+    public final List<Debt> paid;
 
-    public Person(@NonNull long id, @NonNull String name, @NonNull String iban, @NonNull String bic)
-    {
+    public Person(@NonNull long id, @NonNull String name, @NonNull String iban, @NonNull String bic,
+                  @NonNull List<Debt> unpaid, @NonNull List<Debt> paid) {
         this.id = id;
         this.name = name;
         this.iban = iban;
         this.bic = bic;
+        this.unpaid = unpaid;
+        this.paid = paid;
     }
 }
